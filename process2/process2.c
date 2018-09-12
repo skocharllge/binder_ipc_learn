cb108b72dbada846c39ca8880924cc97bb85a119
skocharlge <s.kocharlakota@lge.com>
2018-09-11 19:43:09 -0700

    rewritten from existin file in github

--- process2/process2.c
+++ process2/process2.c
@@ -1,26 +1,3 @@
-/******************************************************************************
-                          ____     ____                                                 
-                 		 / __/	  / __/__  __ _____________                            
-                		/ /_	 _\ \/ _ \/ // / __/ __/ -_|                           
-               			\__/	/___/\___/\___/_/  \__/\__/                            
-                                                                                                                                                                          
-*******************************************************************************
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  
- @file: main.c
- @author: Victor Neville
- @description: Simple program which tests a character device driver. 	  			  
- @date: 13-05-2017
- *****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
